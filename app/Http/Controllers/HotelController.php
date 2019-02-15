862eecaba333532b2ace72f9a75f08f7d772d0ed
AlexandreCamillo <43162926+AlexandreCamillo@users.noreply.github.com>
2019-02-15 15:15:59 -0200

    adicionando upload de imagem ao hotel

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Hotel;
+use Validator;
 
 class HotelController extends Controller
 {
@@ -27,14 +29,51 @@ class HotelController extends Controller
     public function store(Request $request)
     {
         //
+
         $hotel = new Hotel;
         $hotel->name = $request->name;
         $hotel->email = $request->email;
         $hotel->description = $request->description;
         $hotel->telephone = $request->telephone;
         $hotel->address = $request->address;
+
+        
+        //Pega o arquivo da request
+        $file = $request->file('photo');
+
+        //Cria um nome de arquivo único
+        $filename = 'foto.'.$file->getClientOriginalExtension();
+
+        //Cria a pasta de fotos caso não exista
+        if (!Storage::exists('localPhotos/'))
+                    Storage::makeDirectory('localPhotos/',0775,true);
+
+
+        //Valida o arquivo (deveria validar todos os outros parametros de hotel)
+        $validator = Validator::make($request->all(), [
+            'photo' =>'required|file|image|mimes:jpeg,png,gif,webp|max:2048'
+        ]);
+
+        //Caso a requisição venha fora do esperado retorna um erro Bad Request 400
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+        
+        
+        $path = $file->storeAs('localPhotos', $filename);
+
+        $hotel->photo = $path;
+
         $hotel->save();
+
         return response()->json([$hotel]);
+    }
+
+    public function downloadPhoto($id){
+
+        $hotel = Hotel::findOrFail($id);
+
+        return response()->download(storage_path('app/'.$hotel->photo));
     }
 
     /**
@@ -89,3 +128,4 @@ class HotelController extends Controller
     }
 
 }
+
